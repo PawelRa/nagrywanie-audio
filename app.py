28af869bf7da65a2de8d75b5ed318a19bfc3a2e1
PawelRa <pawelrat@poczta.onet.pl>
2024-10-09 10:02:00 +0200

    nagranie audio

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+from io import BytesIO
 import streamlit as st
+from audiorecorder import audiorecorder
 
-st.title("Start")
+#
+#Główna część programu
+#
+st.set_page_config(page_title="Notatki audio", layout="centered")
+
+st.title("Notatki audio")
+note_audio = audiorecorder(
+    start_prompt="Nagraj notatkę",
+    stop_prompt="Zatrzymaj nagrywanie",
+)
+
+if note_audio:
+    audio = BytesIO()
+    note_audio.export(audio, format="mp3")
+    note_audio_bytes = audio.getvalue()
+    st.audio(note_audio_bytes, format="audio/mp3")
+
