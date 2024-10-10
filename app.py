2ae5fa197eeea9a5d759a8599c813506cae14ecd
PawelRa <pawelrat@poczta.onet.pl>
2024-10-10 10:33:39 +0200

    Zamiana audio na text

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from io import BytesIO
 import streamlit as st
-from audiorecorder import audiorecorder
+from io import BytesIO
+from audiorecorder import audiorecorder  # type: ignore
+from dotenv import dotenv_values
+from openai import OpenAI
+
+ENV = dotenv_values(".env")
+
+AUDIO_TRANSCRIBE_MODEL = "whisper-1"
+
+@st.cache_resource
+def get_openai_client():
+    return OpenAI(api_key=ENV["OPENAI_API_KEY"])
+
+def transcribe_audio(audio_bytes):
+    openai_client = get_openai_client()
+    audio_file = BytesIO(audio_bytes)
+    audio_file.name = "audio.mp3"
+    transcript = openai_client.audio.transcriptions.create(
+        file=audio_file,
+        model=AUDIO_TRANSCRIBE_MODEL,
+        response_format="verbose_json",
+    )
+    return transcript.text
 
 #
-#Główna część programu
+# MAIN
 #
-st.set_page_config(page_title="Notatki audio", layout="centered")
+st.set_page_config(page_title="Audio Notatki", layout="centered")
 
-st.title("Notatki audio")
+if "note_audio_bytes" not in st.session_state:
+    st.session_state["note_audio_bytes"] = None
+
+if "note_audio_text" not in st.session_state:
+    st.session_state["note_audio_text"] = ""
+
+st.title("Audio Notatki")
 note_audio = audiorecorder(
     start_prompt="Nagraj notatkę",
     stop_prompt="Zatrzymaj nagrywanie",
@@ -16,6 +43,15 @@ note_audio = audiorecorder(
 if note_audio:
     audio = BytesIO()
     note_audio.export(audio, format="mp3")
-    note_audio_bytes = audio.getvalue()
-    st.audio(note_audio_bytes, format="audio/mp3")
+    st.session_state["note_audio_bytes"] = audio.getvalue()
+    st.audio(st.session_state["note_audio_bytes"], format="audio/mp3")
 
+    if st.button("Transkrybuj audio"):
+        st.session_state["note_audio_text"] = transcribe_audio(st.session_state["note_audio_bytes"])
+
+    if st.session_state["note_audio_text"]:
+        st.text_area(
+            "Transkrypcja audio",
+            value=st.session_state["note_audio_text"],
+            disabled=True,
+        )
